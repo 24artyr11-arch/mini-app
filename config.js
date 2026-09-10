4a4642e616df1eee199faa3109e9e986c36ed761
24artyr11-arch <24artyr11@gmail.com>
2026-09-11 02:21:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Public frontend configuration only. Do not place BOT_TOKEN, API keys,
 // BACKEND_API_KEY, wallet private keys or any other secrets in this file.
 window.MENTAL_TRADER_CONFIG = {
-  API_BASE_URL: "https://github.com/24artyr11-arch/mini1.git",
+  API_BASE_URL: "https://bot-1789074041-2348-www2411.bothost.tech",
   BOT_USERNAME: "mental_traderbot"
 };
